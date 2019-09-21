e28803aad6738625beeffb73f9d93314dcd5d189
cbeard5 <55608329+cbeard5@users.noreply.github.com>
2019-09-21 01:12:52 -0400

    Add files via upload

---
+++ rinjani/rinjani.py
@@ -0,0 +1,19 @@
+
+import time
+
+import os
+import pandas as pd
+import numpy as np
+import time
+import math
+from datetime import datetime
+import yfinance as yf
+
+
+# python package to run machine learning tasks
+
+
+def test_function()
+    return('yeet')
+
+
