7d4b45b524733a77edfec4dc3c0f696c28738a94
cbeard5 <55608329+cbeard5@users.noreply.github.com>
2019-09-20 21:22:23 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/cbeard5/rinjani",
+    url="https://pypi.org/legacy",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
